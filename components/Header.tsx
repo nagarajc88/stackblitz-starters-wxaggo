f2f8dd412632523900bada391cde24f1e34026fe
nagarajc88 <nagarajc1988@gmail.com>
2024-03-14 17:44:34 +0530

    minor changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,47 +28,51 @@ const Header = () => {
 
   return (
     <>
-
-<header>
-      { userLoggedIn == "true" && (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>)}
- </header>
-  <div className="pt-4 pl-2 bg-ct-blue-600 fixed">
-    {store.requestLoading && <Spinner color="text-ct-yellow-600" /> }
-  </div>
+<header className="bg-white h-20">
+        <nav className="h-full flex justify-between container items-center">
+          <div>
+            <Link href="/" className="text-ct-dark-600 text-2xl font-semibold">
+              BCBS
+            </Link>
+          </div>
+          <ul className="flex items-center gap-4">
+            <li>
+              <Link href="/" className="text-ct-dark-600">
+                Home
+              </Link>
+            </li>
+            {!user && (
+              <>
+                <li>
+                  <Link href="/register" className="text-ct-dark-600">
+                    Register
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/login" className="text-ct-dark-600">
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li>
+                  <Link href="/profile" className="text-ct-dark-600">
+                    Profile
+                  </Link>
+                </li>
+                <li className="cursor-pointer" onClick={handleLogout}>
+                  Logout
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </header>
+      { store.requestLoading && ( <div className="pt-4 pl-2 bg-ct-blue-600 fixed">
+         {store.requestLoading && <Spinner color="text-ct-yellow-600" />}
+      </div>)} 
 </>);
 };
 
