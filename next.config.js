bcf881c28b3a35af46cb078635c27fe47cb856df
nagarajc88 <nagarajc1988@gmail.com>
2024-03-14 13:57:19 +0530

    env files added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+
+function loadTargetEnv(target) {
+  const envPath = path.join(__dirname, 'env', `.env.${target}`);
+  return dotenv.parse(fs.readFileSync(envPath));
+}
+
 const nextConfig = {
+    env: loadTargetEnv(process.env.TARGET_ENVIRONMENT),
     async redirects() {
         return [
             {
@@ -8,7 +19,7 @@ const nextConfig = {
                 permanent: true,
             },
         ];
-   },
+    },
 };
 
 module.exports = nextConfig;
